45d3584506e748ee7d4fb2b33285512f6fc2795c
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-09-05 07:07:53 -0400

    Exclude gg_ file. Correct database exists if statement

--- blastBacteria.bash
+++ blastBacteria.bash
@@ -5,13 +5,14 @@ set -e
 #wget -r --no-parent --no-directories ftp://ftp.ensemblgenomes.org/pub/release-32/bacteria//fasta/ -A dna.toplevel.fa.gz
 
 echo Making bacteria blast db
-if [ -e bacteria/bacteria.00.nin ];then
+if [ ! -e bacteria/bacteria.00.nin ];then
   cd bacteria
   find -name '*.gz' -exec zcat {} \; |makeblastdb -dbtype nucl -out bacteria -title bacteria
   cd ..
 fi
 
-for ii in /media/THING1/alexandra/4Gut_Phage_and_Bacteria_DNA_Modifications/014Blast16s/*.fasta;do
+#exclude large gg_ sample
+for ii in /media/THING1/alexandra/4Gut_Phage_and_Bacteria_DNA_Modifications/014Blast16s/[^g]*.fasta;do
   outfile=work/$(basename $ii|sed 's/\.fasta$//').bacteria.blast.gz
   if [ -e "$outfile" ];then
     echo $outfile already exists
